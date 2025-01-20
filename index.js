12654c9bcab3dc1a7306a940e10dc313f4ed45db
Lucio Schraier <lucio.ese@gmail.com>
2025-01-20 16:46:47 +0000

    feat: Adding exercises are now implemented

--- index.js
+++ index.js
@@ -17,13 +17,19 @@ app.get('/', (req, res) => {
 
 const userSchema = new Schema({
   username: {type: String, required: true},
+  log: [{
+    description: String,
+    duration: Number,
+    date: String,
+  }],
+  count: {type: Number, default: 0},
 });
 
 const User = mongoose.model('User', userSchema);
 
 app.route("/api/users")
   .post((req, res) => {
-    let user = new User({username: req.body.username})
+    let user = new User({ username: req.body.username })
 
     user.save()
       .then(data => res.json(data))
@@ -35,9 +41,23 @@ app.route("/api/users")
       .catch(err => console.error(err))
   })
 
+app.post("/api/users/:_id/exercises", (req, res) => {
+  const { description, duration } = req.body;
+  const date = req.body.date ? new Date(req.body.date).toDateString() : new Date().toDateString();
+  const id = req.params._id;
 
-
-
+  const exercise = {
+    description,
+    duration: parseInt(duration),
+    date,
+  }
+  
+  User.findByIdAndUpdate(id, { $push: { log: exercise }, $inc: { count: 1 } }, { new: true })
+    .then(user => {
+      res.json({ username: user.username, ...exercise, _id: id})
+    })
+    .catch(err => console.error(err))
+})
 
 app.get("/mongo-health", (req, res) => {
   res.json({status: mongoose.connection.readyState})
